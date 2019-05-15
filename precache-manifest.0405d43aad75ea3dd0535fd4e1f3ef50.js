e85aafb48e37b9e5d5c5d9353b25f346199a82ca
rporopat <poropat.roberto@gmail.com>
2019-05-15 14:23:15 +0200

    First commit

---
+++ precache-manifest.0405d43aad75ea3dd0535fd4e1f3ef50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77fb7928f8387012e8a130d722291878",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b623c7838ad0166612f4",
+    "url": "/robofriends/static/css/main.27f90a12.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "b623c7838ad0166612f4",
+    "url": "/robofriends/static/js/main.8d0fad86.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
